6d659b01ea79bf8682d815d4aa1c85f99e8f301b
Raj Jaiswal <raj.markts@gmail.com>
2026-05-05 22:38:25 +0530

    fixed next config syntax for es modules

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Pin Turbopack workspace root so Vercel doesn't infer it from parent lockfiles.
-  turbopack: {
-    root: process.cwd(),
-  },
-};
-
-export default nextConfig;
